75d1a1c3fd45e8e99378a6b5c572b9751a1f7530
tejas-gupta-dev <tejasgupta1512@gmail.com>
2026-01-04 05:27:42 +0530

    Update App.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: true,
+    origin: "https://zomagram.vercel.app/",
     credentials: true
 }));
 app.use(express.json());
