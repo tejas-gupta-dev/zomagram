71a3062586986b8b7d64d22bb5976bd206798a19
tejas-gupta-dev <tejasgupta1512@gmail.com>
2026-01-04 05:55:43 +0530

    Update FoodPartnerRegister.jsx

--- frontend/src/pages/auth/FoodPartnerRegister.jsx
+++ frontend/src/pages/auth/FoodPartnerRegister.jsx
@@ -18,7 +18,7 @@ const FoodPartnerRegister = () => {
     const password = e.target.password.value;
     const address = e.target.address.value;
 
-    axios.post(`${API_URL}/api/auth/food-partner/register`, {
+    axios.post("https://zomagram.onrender.com/api/auth/food-partner/register", {
       name:businessName,
       contactName,
       phone,
