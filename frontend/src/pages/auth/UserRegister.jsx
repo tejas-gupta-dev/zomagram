4cc9c97f2a03884da612a50101709e4293c68dc0
tejas-gupta-dev <tejasgupta1512@gmail.com>
2026-01-04 05:56:42 +0530

    Update UserRegister.jsx

--- frontend/src/pages/auth/UserRegister.jsx
+++ frontend/src/pages/auth/UserRegister.jsx
@@ -18,7 +18,7 @@ const UserRegister = () => {
         const password = e.target.password.value;
 
 
-        const response = await axios.post(`${API_URL}/api/auth/user/register`, {
+        const response = await axios.post("https://zomagram.onrender.com/api/auth/user/register", {
             fullName: firstName + " " + lastName,
             email,
             password
